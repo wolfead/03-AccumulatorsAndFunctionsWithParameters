a0e0a598fa7606f0c0da89b433a141de6989656c
wolfead <wolfead@rose-hulman.edu>
2018-11-29 16:08:15 -0500

    completed todo's

--- src/m7_summing.py
+++ src/m7_summing.py
@@ -4,8 +4,8 @@ in its simplest classic forms:
    SUMMING:       total = total + number
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Alex Wolfe.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -17,7 +17,7 @@ def main():
 def run_test_sum_cosines():
     """ Tests the   sum_cosines   function. """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement this function.
+    # done: 2. Implement this function.
     #   It TESTS the  sum_cosines  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -28,7 +28,28 @@ def run_test_sum_cosines():
     print('--------------------------------------------------')
     print('Testing the   sum_cosines   function:')
     print('--------------------------------------------------')
+#Test 1
+    expected = 0.13416
+    answer = sum_cosines(3)
+    print('returned', answer)
+    print('expected', expected)
 
+    #Test 2
+    expected = -0.5194806481
+    answer = sum_cosines(4)
+    print('expected', expected)
+    print('returned', answer)
+
+    #Test 3
+    expected = -0.4174477465
+    answer = sum_cosines(10)
+    print('expected',expected)
+    print('returned', answer)
+    print('+++++++++++++++++++++++++++')
+    print('---------------------------')
+    print('~~~~~YYYAAAAAAAYYYY~~~~~~~~')
+    print('---------------------------')
+    print('+++++++++++++++++++++++++++')
 
 def sum_cosines(n):
     """
@@ -41,19 +62,26 @@ def sum_cosines(n):
         cos(0) + cos(1) + cos(2) + cos(3)   which is about 0.13416.
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # done: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
     #   No fair running the code of  sum_cosines  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # -------------------------------------------------------------------------
+    import math
+    total = 0
+    for k in range(n+1):
+        total = total + math.cos(k)
+    return total
+
+
 
 
 def run_test_sum_square_roots():
     """ Tests the   sum_square_roots   function. """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement this function.
+    # done: 4. Implement this function.
     #   It TESTS the  sum_square_roots  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -64,8 +92,23 @@ def run_test_sum_square_roots():
     print('--------------------------------------------------')
     print('Testing the   sum_square_roots   function:')
     print('--------------------------------------------------')
+#Test 1
+    expected = 11.854408
+    answer = sum_square_roots(5)
+    print('expected', expected)
+    print('returned', answer)
 
+    #Test 2
+    expected = 3.414213562
+    answer = sum_square_roots(2)
+    print('expected', expected)
+    print('returned', answer)
 
+    #Test 3
+    EXPECTED = 5.863703305
+    ANSWER = sum_square_roots(3)
+    print('expected', EXPECTED)
+    print('returned', ANSWER)
 def sum_square_roots(n):
     """
     What comes in:  A non-negative integer n.
@@ -80,13 +123,20 @@ def sum_square_roots(n):
       which is about 11.854408.
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # done: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
     #   No fair running the code of  sum_square_roots  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # -------------------------------------------------------------------------
+    import math
+    total = 0
+    for k in range(n+1):
+        total = total + math.sqrt(2*k)
+
+    return total
+
 
 
 # -----------------------------------------------------------------------------
